a84c43a6b00315907fe2844c22d9af3da18505e7
Elias Franz <ecfranz@live.com>
2017-01-21 12:01:34 -0500

    table schema size checking (inc)

--- schema.js
+++ schema.js
@@ -4,14 +4,23 @@ var check;
 db.serialize(function() {
 
   db.run("CREATE TABLE if not exists user_info (info TEXT)");
-  var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
 
+  var freshStart;
+  db.all("SELECT * FROM user_info", function(err,row) {
+      if (console.log("EF rowtype: " + row.type)){
+        freshStart = true;
+      }
+  });
+
+  if(freshStart){
+      var stmt = db.prepare("INSERT INTO user_info VALUES (?)");
+      for (var i = 0; i < 10; i++) {
+          stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
+  }
   db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
-      console.log(row.id + ":, " + row.info);
+      console.log(row.id + ": " + row.info);
   });
 });
 
